272b36df6322b88647036ab516027f40a3fcce6b
Ryan Pickett <ryan@hdpe.me>
2015-05-29 23:38:11 +0100

    Provide simple step completion content assist.

--- cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/Editor.java
+++ cucumber.eclipse.editor/src/main/java/cucumber/eclipse/editor/editors/Editor.java
@@ -1,5 +1,8 @@
 package cucumber.eclipse.editor.editors;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,6 +35,7 @@ import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 import cucumber.eclipse.editor.Activator;
 import cucumber.eclipse.editor.markers.MarkerManager;
 import cucumber.eclipse.editor.steps.ExtensionRegistryStepProvider;
+import cucumber.eclipse.editor.steps.IStepProvider;
 import cucumber.eclipse.steps.integration.Step;
 
 public class Editor extends TextEditor {
@@ -75,6 +79,7 @@ public class Editor extends TextEditor {
 	private ProjectionAnnotationModel annotationModel;
 	private Annotation[] oldAnnotations;
 	private GherkinOutlinePage outlinePage;
+	private IStepProvider stepProvider;
 	
 	/*
 	 * (non-Javadoc)
@@ -105,6 +110,10 @@ public class Editor extends TextEditor {
 		}
 	}
 	
+	TextSelection getSelection() {
+		return (TextSelection) getSelectionProvider().getSelection();
+	}
+	
 	public void updateGherkinModel(GherkinModel model) {
 		updateOutline(model.getFeatureElement());
 		updateFoldingStructure(model.getFoldRanges());
@@ -126,8 +135,7 @@ public class Editor extends TextEditor {
 	}
 	
 	private String getSelectedLine() {
-		TextSelection selecton = (TextSelection) getSelectionProvider().getSelection();
-		int line = selecton.getStartLine();
+		int line = getSelection().getStartLine();
 		
 		IDocumentProvider docProvider = getDocumentProvider();
 		IDocument doc = docProvider.getDocument(getEditorInput());
@@ -138,6 +146,16 @@ public class Editor extends TextEditor {
 	public Step getStep() {
 		String selectedLine = getSelectedLine();
 		return model.getStep(selectedLine);
+	}
+
+	public List<Step> getAvailableSteps() {
+		List<Step> steps = new ArrayList<Step>(stepProvider.getStepsInEncompassingProject());
+		Collections.sort(steps, new Comparator<Step>() {
+			public int compare(Step o1, Step o2) {
+				return o1.getText().compareTo(o2.getText());
+			}
+		});
+		return steps;
 	}
 
 	@Override
@@ -160,8 +178,8 @@ public class Editor extends TextEditor {
 		if (model != null) {
 			model.dispose();
 		}
-		model = new GherkinModel(new ExtensionRegistryStepProvider(input.getFile()),
-				new MarkerManager(), input.getFile());
+		stepProvider = new ExtensionRegistryStepProvider(input.getFile());
+		model = new GherkinModel(stepProvider, new MarkerManager(), input.getFile());
 	}
 
 	public void dispose() {
